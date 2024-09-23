43e27bd0f9c6b845831f8a6911744a9728d3a8e4
kimwoo123 <52848811+kimwoo123@users.noreply.github.com>
2024-09-23 16:36:14 +0900

    [level 2] Title: 방문 길이, Time: 0.53 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,16 +1,15 @@
 def solution(dirs):
     answer = 0
-    dir_map = {
-        "U": 0,
-        "L": 1,
-        "R": 2,
-        "D": 3,
+    delta_map = {
+        "U": (-1, 0),
+        "L": (0, -1),
+        "R": (0, 1),
+        "D": (1, 0),
     }
-    delta = [(-1, 0), (0, -1), (0, 1), (1, 0)]
     visited = set()
     row, col = 0, 0
     for dir_ in dirs:
-        delta_row, delta_col = delta[dir_map[dir_]]
+        delta_row, delta_col = delta_map[dir_]
         next_row = row + delta_row
         next_col = col + delta_col
         if -5 <= next_row <= 5 and -5 <= next_col <= 5:
