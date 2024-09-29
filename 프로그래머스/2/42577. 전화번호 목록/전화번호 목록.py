c3a835a892d56efc87fdd49844e02884b64adcaf
kimwoo123 <52848811+kimwoo123@users.noreply.github.com>
2024-09-29 10:56:48 +0900

    [level 2] Title: 전화번호 목록, Time: 399.67 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,14 +1,11 @@
-def strcmp(s1, s2):
-    l = min(len(s1), len(s2))
-    for i in range(l):
-        if s1[i] != s2[i]:
-            return False
-    return True
-
 def solution(phone_book):
-    phone_book.sort()
-    for i in range(len(phone_book) - 1):
-        if strcmp(phone_book[i], phone_book[i+1]):
-            return False
+    
+    number_set = set(phone_book)
+    for number in phone_book:
+        prefix = ""
+        for n in number:
+            prefix += n
+            if prefix in number_set and prefix != number:
+                return False
     
     return True
