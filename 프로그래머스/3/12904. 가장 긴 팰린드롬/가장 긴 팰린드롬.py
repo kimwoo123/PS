62aa0d65da39835d64002316368115481f07bd6e
kimwoo123 <52848811+kimwoo123@users.noreply.github.com>
2024-09-30 13:23:35 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 2812.56 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -1,10 +1,7 @@
 def solution(s):
-    answer = 0
     l = len(s)
     for j in range(l, 0, -1):
         for i in range(l - j + 1):
             temp = s[i:i+j]
             if temp == temp[::-1]:
                 return j
-
-    return answer
