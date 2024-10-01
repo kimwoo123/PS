aaf484d9b29f8ff3e16a9665c77615cfa7a93bd9
kimwoo123 <52848811+kimwoo123@users.noreply.github.com>
2024-10-01 11:09:21 +0900

    [level 3] Title: 거스름돈, Time: 444.43 ms, Memory: 13.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.py
@@ -0,0 +1,11 @@
+def solution(n, money):
+    answer = 0
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    for m in money:
+        for i in range(m, n+1):
+            dp[i] += dp[i-m]
+        
+    
+    return dp[-1] % 1000000007
+
