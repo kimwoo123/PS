e045bdd09aabb7ee808d150ac42dd85f5afd5338
kimwoo123 <52848811+kimwoo123@users.noreply.github.com>
2024-10-02 19:58:17 +0900

    [level 3] Title: 합승 택시 요금, Time: 2353.43 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -0,0 +1,19 @@
+def solution(n, s, a, b, fares):    
+    matrix = [[1000001] * (n+1) for _ in range(n+1)]
+    for i, j, cost in fares:
+        matrix[i][j] = cost
+        matrix[j][i] = cost
+    
+    for i in range(n+1):
+        matrix[i][i] = 0
+        
+    for k in range(n+1):
+        for i in range(n+1):
+            for j in range(n+1):
+                matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[j][k])
+        
+    answer = 100000001
+    for i in range(1, n+1):
+        answer = min(answer, matrix[i][s] + matrix[i][a] + matrix[i][b])
+        
+    return answer
