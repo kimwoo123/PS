85b023d369c30ae60ee4c0371850ef83defe3966
kimwoo123 <52848811+kimwoo123@users.noreply.github.com>
2024-10-07 10:44:33 +0900

    [level 2] Title: 테이블 해시 함수, Time: 89.73 ms, Memory: 64.3 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/README.md
+++ 프로그래머스/2/147354. 테이블 해시 함수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 테이블 해시 함수 - 147354 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147354#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147354) 
 
 ### 성능 요약
 
-메모리: 64.1 MB, 시간: 41.10 ms
+메모리: 64.3 MB, 시간: 89.73 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 07일 10:42:00
+2024년 10월 07일 10:44:32
 
 ### 문제 설명
 
--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -1,14 +1,11 @@
 def solution(data, col, row_begin, row_end):
     data.sort(key=lambda x:(x[col-1], -x[0]))
-    targets = data[row_begin-1: row_end]
     
-    i = row_begin
     answer = 0
-    for t in targets:
+    for i in range(row_begin-1, row_end):
         total = 0
-        for n in t:
-            total += (n%i)
+        for n in data[i]:
+            total += n % (i+1)
         answer ^= total
-        i += 1
     
     return answer
