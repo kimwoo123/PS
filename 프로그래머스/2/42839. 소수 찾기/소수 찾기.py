9e8c0c433866deca71fc37320f2088ca827d61d8
kimwoo123 <52848811+kimwoo123@users.noreply.github.com>
2024-10-09 14:44:39 +0900

    [level 2] Title: 소수 찾기, Time: 2169.52 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,32 @@
+from math import sqrt
+
+def get_permutations(numbers, visited, index, l, ret):
+    if index == 0:
+        yield int(ret)
+    for i in range(l):
+        if visited[i] == 0:
+            visited[i] = 1
+            yield from get_permutations(numbers, visited, index - 1, l, ret + numbers[i])
+            visited[i] = 0
+    
+def solution(numbers):
+    l = len(numbers)
+    ll = 10 ** l
+    
+    erasto = [1] * ll
+    erasto[0], erasto[1] = 0, 0
+    end = int(sqrt(ll)) + 1
+    for i in range(2, end):
+        if erasto[i] == 0:
+            continue
+        for j in range(i * 2, ll, i):
+            erasto[j] = 0
+            
+    answer = set()
+    visited = [0] * l
+    for i in range(1, l+1):
+        for perm in get_permutations(numbers, visited, i, l, ''):
+            if erasto[perm] and perm not in answer:
+                answer.add(perm)
+            
+    return len(answer)
