922a1abf77cf3d729ea9499da86517b06c35ec9f
kimwoo123 <52848811+kimwoo123@users.noreply.github.com>
2024-10-11 10:23:46 +0900

    [Gold III] Title: 줄 세우기, Time: 152 ms, Memory: 38308 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+def solution():
+    N, M = map(int, input().split())
+
+    indegree = [0] * (N + 1)
+    graph = [[] for _ in range(N+1)]
+    for _ in range(M):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        indegree[b] += 1
+
+    queue = []
+    for i in range(1, N+1):
+        if indegree[i] == 0:
+            queue.append(i)
+
+    answer = []
+    while queue:
+        node = queue.pop(0)
+        answer.append(node)
+        for next_node in graph[node]:
+            indegree[next_node] -= 1
+            if indegree[next_node] == 0:
+                queue.append(next_node)
+
+    print(*answer)
+
+if __name__ == "__main__":
+   solution()
