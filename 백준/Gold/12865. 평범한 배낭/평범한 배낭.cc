0809c51d3ccbbe129376636faea6b38a02f69b00
kimwoo123 <52848811+kimwoo123@users.noreply.github.com>
2024-10-17 11:41:51 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 1500 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -2,33 +2,21 @@
 #include <algorithm>
 
 int N, K;
-int weights[100], values[100];
-int dp[101][100001];
+int dp[100001];
 
 void solve(void) {
     int w, v;
-    for (int i = 1; i < N+1; ++i){
-        w = weights[i-1];
-        v = values[i-1];
-        for (int j = 1; j < K+1; ++j) {
-            if (j < w)
-                dp[i][j] = dp[i-1][j];
-            else
-                dp[i][j] = std::max(dp[i-1][j], v + dp[i-1][j-w]);
+    for (int i = 0; i < N; ++i){
+        scanf("%d %d", &w, &v);
+        for (int j = K; j >= w; --j) {
+            dp[j] = std::max(dp[j-w] + v, dp[j]);
         }
     }
-    printf("%d", dp[N][K]);
+    printf("%d", dp[K]);
 }
 
 void input_data(void) {
     scanf("%d %d", &N, &K);
-
-    int v, w;
-    for (int i = 0; i < N; ++i) {
-        scanf("%d %d", &w, &v);
-        weights[i] = w;
-        values[i] = v;
-    }
 }
 
 int main(void) {
