16560c697d2d0516b5938708f88aad143858a020
kimwoo123 <52848811+kimwoo123@users.noreply.github.com>
2024-10-23 10:26:52 +0900

    [Silver II] Title: 한 줄로 서기, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -3,40 +3,8 @@ import java.util.*;
 
 
 public class Main {
-    static boolean[] visited;
     static int[] info;
     static int N;
-
-    private static void combination(int index, int len, Stack<Integer> result) {
-        if (index == len) {
-            int count, target;
-            for (int i = 0; i < N; ++i) {
-                count = 0;
-                target = result.get(i);
-                for (int j = 0; j < i; ++j) {
-                    if (result.get(j) > target) ++count;
-                }
-
-                if (info[target-1] != count) return;
-            }
-
-            StringBuilder sb = new StringBuilder();
-            for (int i: result)
-                sb.append(i).append(" ");
-            System.out.print(sb);
-        }
-        else {
-            for (int i = 1; i < len + 1; ++i) {
-                if (visited[i] == false) {
-                    visited[i] = true;
-                    result.add(i);
-                    combination(index + 1, len, result);
-                    result.pop();
-                    visited[i] = false;
-                }
-            }
-        }
-    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -50,7 +18,24 @@ public class Main {
             info[i] = Integer.parseInt(tokenizer.nextToken());
         }
 
-        visited = new boolean[N+1];
-        combination(0, N, new Stack<>());
+        int[] result = new int[N];
+        int count;
+        for (int i = 0; i < N; ++i) {
+            count = 0;
+            for (int j = 0; j < N; ++j) {
+                if (count == info[i] && result[j] == 0) {
+                    result[j] = i+1;
+                    break;
+                }
+                else if (result[j] == 0)
+                    ++count;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; ++i)
+            sb.append(result[i]).append(" ");
+
+        System.out.print(sb);
     }
 }
